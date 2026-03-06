05f2c80571c3bfa3e650b80e9978cd36f7b3b0d6
Ronald <s1208278@student.windesheim.nl>
2026-03-06 20:38:02 +0100

    duidelijker error meldingen

--- Portfoliowebsite/wwwroot/js/site.js
+++ Portfoliowebsite/wwwroot/js/site.js
@@ -8,6 +8,8 @@ function setupValidation() {
     const email = document.getElementById('Email');
     const name = document.getElementById('Name');
     const msg = document.getElementById('Message');
+    const submit = document.getElementsByClassName('Message');
+    let errors = [0, 0, 0]
 
     const echo = (id, value) => {
         document.getElementById(id).innerHTML = `\n <span>Probleem met: ${value}</span>\n `;
@@ -23,17 +25,20 @@ function setupValidation() {
             if (el === email) {
                 reset("emailErr")
                 if (!naiveEmailCheck(el.value)) {
-                    echo('emailErr', el.value);
+                    error = `email, hij is niet geldig`
+                    echo('emailErr', error);
                 }
             } else if (el === name) {
                 reset("nameErr")
                 if (el.value.length < 2) {
-                    echo('nameErr', el.value);
+                    error = `naam, hij is te kort`
+                    echo('nameErr', error);
                 }
             } else if (el === msg) {
                 reset("msgErr")
                 if (el.value.length < 5) {
-                    echo('msgErr', el.value);
+                    error = `bericht, hij is te kort`
+                    echo('msgErr', error);
                 }
             }
         });
